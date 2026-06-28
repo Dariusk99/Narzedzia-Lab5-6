71dfc077e37570bd30a04a38bea3ddd69000cf99
Dariusk99 <dariuszkosnicki@gmail.com>
2026-06-28 05:03:16 +0200

    TASK2: add load json

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 parser = argparse.ArgumentParser()
 
@@ -9,3 +10,24 @@ args = parser.parse_args()
 
 print("Plik wejściowy:", args.input)
 print("Plik wyjściowy:", args.output)
+
+# Wczytaj JSON
+def read_json(path):
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        print("Wczytano plik JSON")
+        return data
+
+    except FileNotFoundError:
+        print("Plik JSON nie istnieje")
+        return None
+
+    except json.JSONDecodeError:
+        print("Niepoprawna składnia pliku JSON")
+        return None
+
+    except PermissionError:
+        print("Brak uprawnień do pliku JSON")
+        return None
